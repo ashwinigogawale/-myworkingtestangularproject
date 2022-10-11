cdb51fb8efafbad406fe99a3fcdd998444c90e51
ashwinikaddekatc <ashwini.kad@dekatc.com>
2022-10-11 16:43:42 +0530

    gmart-master

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { SharedModule } from 'shared/shared.module';
+import { environment } from '../environments/environment';
+import { AdminModule } from './admin/admin.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingModule } from './shopping/shopping.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AdminModule,
+    ShoppingModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCmT7qy9nKyyl8dxkvWwFJFGgd3lZmFcS8",
+    authDomain: "gmart-c.firebaseapp.com",
+    databaseURL: "https://gmart-c.firebaseio.com",
+    projectId: "gmart-c",
+    storageBucket: "gmart-c.appspot.com",
+    messagingSenderId: "1023344527893",
+    appId: "1:1023344527893:web:e64ffd2f002fa0a3"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
