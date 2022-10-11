70024b5c2e065375b0781ec5595b95323d087ebd
ashwinikaddekatc <ashwini.kad@dekatc.com>
2022-10-11 16:26:16 +0530

    saheli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlouseComponent } from './blouse/blouse.component';
+import { DressesComponent } from './dresses/dresses.component';
+import { FallComponent } from './fall/fall.component';
+import { KidsComponent } from './kids/kids.component';
+import { LongfComponent } from './longf/longf.component';
+import { SareeComponent } from './saree/saree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'saree', component:SareeComponent },
+  { path: 'one', component:LongfComponent},
+  { path: 'blouses', component:BlouseComponent},
+  { path: 'dresses', component:DressesComponent},
+  { path: 'kids', component:KidsComponent},
+  { path: 'fallpico', component:FallComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
